3b6045b0c447126da34175ff013f75e53a6924e9
Jake Squelch <jakewsquelch@gmail.com>
2025-01-29 15:30:02 +0000

    Debugged using a breakpoint

--- ConsoleCalculator.cpp
+++ ConsoleCalculator.cpp
@@ -15,12 +15,22 @@ int main()
     cout << "Calculator Console Application\n\n";
     cout << "Please enter the operation to perform. Format: a+b | a-b | a*b | a/b" << endl;
 
-    Calculator c;
+    //defining an object of Calculator called c
+    Calculator c; 
     while (true)
     {
         cin >> x >> oper >> y;
-        result = c.Calculate(x, oper, y);
-        cout << "Result " << "of " << x << oper << y << " is: " << result << endl;
+        if (oper == '/' && y == 0) 
+        {
+            cout << "Cannot divide by 0, please try again!\n";
+            continue;
+        }
+        else 
+        {
+            result = c.Calculate(x, oper, y);
+            //we have added a conditional breakpoint above, so that the app pauses on the above line when we try to divide by 0
+            cout << "Result " << "of " << x << oper << y << " is: " << result << endl;
+        }
     }
     return 0;
 }
