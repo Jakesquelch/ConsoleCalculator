d8b59bab6badf4ae859a17277bcbb0b556547d0e
Jake Squelch <jakewsquelch@gmail.com>
2025-01-29 15:00:28 +0000

    Defined the calculator function

--- Calculator.cpp
+++ Calculator.cpp
@@ -1,6 +1,24 @@
+
 #include "Calculator.h"
+
+#include <iostream>
+
+using namespace std;
 
 double Calculator::Calculate(double x, char oper, double y)
 {
-    return 0.0;
+    switch (oper)
+    {
+        case '+':
+            return x + y;
+        case '-':
+            return x - y;
+        case '*':
+            return x * y;
+        case '/':
+            return x / y;
+        default:
+            cout << "Error: Enter a valid operation";
+            return 0.0;
+    }
 }
